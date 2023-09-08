c9f27ad2a473e92a8d8cbd6363f459d69bff9ada
SriramKumar <gode.srk@gmail.com>
2023-09-08 06:57:48 +0530

    WiseSplitSpringBootProject

---
+++ src/main/java/com/scaler/Splitwise/controller/SettleUpController.java
@@ -0,0 +1,9 @@
+package com.scaler.Splitwise.controller;
+
+// We are creating API in the controller layer.
+// If it is a normal controller we write the annotation as @Controller
+// if it is a rest api controller, we use @RestController
+public class SettleUpController {
+
+
+}
